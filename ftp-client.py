3a19fb17b09870191daeb808dcf77f2ef7ecf51c
BBrel <vbrelgin@gmail.com>
2024-06-09 14:38:41 +0300

    my_version

--- ftp-client.py
+++ ftp-client.py
@@ -1,17 +1,47 @@
 import socket
 
-HOST = 'localhost'
-PORT = 6666
 
-while True:
-    request = input('>')
-    
-    sock = socket.socket()
-    sock.connect((HOST, PORT))
-    
-    sock.send(request.encode())
-    
-    response = sock.recv(1024).decode()
-    print(response)
-    
-    sock.close()
+class FileClient:
+    def __init__(self, host='localhost', port=8080):
+        self.host = host
+        self.port = port
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def send_message(self, message):
+        self.client_socket.send(message.encode())
+        response = self.client_socket.recv(4096).decode()
+        return response
+
+    def receive_file(self, filepath):
+        with open(filepath, 'wb') as file:
+            file_data = self.client_socket.recv(4096)
+            file.write(file_data)
+        print("Файл успешно скачан.")
+
+    def run(self):
+        self.client_socket.connect((self.host, self.port))
+        print("Успешно подключено к серверу.")
+
+        print("Введите команды для управления файлами на сервере:")
+        while True:
+            message = input(" -> ")
+            if message.lower() == 'exit':
+                response = self.send_message(message)
+                print('Ответ сервера: ' + response)
+                break
+
+            elif message.lower().startswith('copyfrom'):
+                _, filename = message.split()
+                self.send_message(message)
+                self.receive_file(filename)
+
+            else:
+                response = self.send_message(message)
+                print('Ответ сервера: ' + response)
+
+        self.client_socket.close()
+        print("Соединение закрыто.")
+
+
+client = FileClient()
+client.run()
--- ftp-server.py
+++ ftp-server.py
@@ -1,35 +1,76 @@
 import socket
+import threading
 import os
-'''
-pwd - показывает название рабочей директории
-ls - показывает содержимое текущей директории
-cat <filename> - отправляет содержимое файла
-'''
-
-dirname = os.path.join(os.getcwd(), 'docs')
-
-def process(req):
-    if req == 'pwd':
-        return dirname
-    elif req == 'ls':
-        return '; '.join(os.listdir(dirname))
-    return 'bad request'
+import shutil
 
 
-PORT = 6666
+def client_thread(conn, ip, port):
+    base_dir = "server_workdir"
 
-sock = socket.socket()
-sock.bind(('', PORT))
-sock.listen()
-print("Прослушиваем порт", PORT)
+    while True:
+        try:
+            # Получение данных от клиента
+            data = conn.recv(4096).decode('utf-8').strip()
+            if not data: break  # выход, если данные пусты
 
-while True:
-    conn, addr = sock.accept()
-    
-    request = conn.recv(1024).decode()
-    print(request)
-    
-    response = process(request)
-    conn.send(response.encode())
+            print(f"Received from {ip}:{port} - {data}")
+            command, *args = data.split()
 
-conn.close()
+            if command == 'LS':  # Посмотреть содержимое папки
+                response = '\n'.join(os.listdir(base_dir)) or "Папка пуста"
+
+            elif command == 'MKDIR':  # Создать папку
+                os.makedirs(os.path.join(base_dir, *args), exist_ok=True)
+                response = "Папка создана"
+
+            elif command == 'RMDIR':  # Удалить папку
+                shutil.rmtree(os.path.join(base_dir, *args))
+                response = "Папка удалена"
+
+            elif command == 'RM':  # Удалить файл
+                os.remove(os.path.join(base_dir, *args))
+                response = "Файл удален"
+
+            elif command == 'MV':  # Переименовать файл
+                shutil.move(os.path.join(base_dir, args[0]), os.path.join(base_dir, args[1]))
+                response = "Файл переименован"
+
+            elif command == 'COPYFROM':  # Скопировать файл с сервера на клиент
+                filepath = os.path.join(base_dir, args[0])
+                with open(filepath, 'rb') as f:
+                    conn.sendall(f.read())
+                continue
+
+            elif command == 'EXIT':  # Выход
+                response = "Вы отключены от сервера"
+                conn.send(response.encode('utf-8'))
+                break
+
+            else:
+                response = "Неизвестная команда"
+
+            # Отправка ответа клиенту
+            conn.send(response.encode('utf-8'))
+
+        except Exception as e:
+            print(f"Error handling {ip}:{port} - {e}")
+            conn.send(str(e).encode('utf-8'))
+    conn.close()  # закрытие соединения
+    print(f"Connection {ip}:{port} has been closed.")
+
+
+def start_server(host="0.0.0.0", port=8080):
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    soc.bind((host, port))
+    soc.listen(10)
+    print(f"Socket now listening")
+
+    while True:
+        conn, addr = soc.accept()
+        ip, port = str(addr[0]), str(addr[1])
+        print(f"Accepting connection from {ip}:{port}")
+        threading.Thread(target=client_thread, args=(conn, ip, port)).start()
+
+
+start_server()
